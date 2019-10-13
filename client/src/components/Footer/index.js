145e8f98e559680d6b173a77503b04c9b1dc6096
gundrm <rachelm.gundy@gmail.com>
2019-10-13 13:18:17 -0700

    client folder working

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import "./style.css";
+
+const Footer = () => (
+	<footer className = "page-footer">
+		<div className = "container-fluid text-center">
+			<p>&copy; Copyright 2019</p>
+		</div>	
+	</footer>
+);
+
+export default Footer;
